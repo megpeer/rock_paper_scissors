5adbf46aec79089365efcb7bab18274c5347377f
Meg Peer <megimu@gmail.com>
2024-04-26 08:34:26 -0400

    game winner call added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // prompt player choice
 //get random computer choice
 //play out the round
-//score a win for the cpmputer or player
+//score a win for the computer or player
 // print # of rounds played
 
-// loop until player or computer get 5 points
+// loop 5 times
 
 
 
@@ -48,19 +48,19 @@ function playRound(playerSelection, computerSelection) {
     result = console.log('Tie!');}
 
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    result = playerWinCount+=1; console.log('You win! Rock beats Scissors! (rock blunts scissors)');}
+    result = playerWinCount+=1, console.log('You win! Rock beats Scissors! (rock blunts scissors)');}
     
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    result = playerWinCount+=1; console.log('You win! Scissors beats Paper! (scissors cuts paper)'); }
+    result = playerWinCount+=1, console.log('You win! Scissors beats Paper! (scissors cuts paper)'); }
 
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    result = playerWinCount+=1; console.log('You win! Paper beats Rock! (paper covers rock)');}
+    result = playerWinCount+=1, console.log('You win! Paper beats Rock! (paper covers rock)');}
 
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    result = computerWinCount+=1; console.log('You lose! Rock beats Scissors! (rock blunts scissors)'); }
+    result = computerWinCount+=1, console.log('You lose! Rock beats Scissors! (rock blunts scissors)'); }
 
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    result =  computerWinCount+=1; console.log('You lose! Scissors beats Paper! (scissors cuts paper)'); }
+    result =  computerWinCount+=1, console.log('You lose! Scissors beats Paper! (scissors cuts paper)'); }
 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
     result =  computerWinCount+=1, console.log('You lose! Paper beats Rock! (paper covers rock)');}
@@ -79,3 +79,19 @@ console.log(`you have played ${rounds} rounds`)
 }
 
 playGame()
+
+function winner(playerWinCount, computerWinCount) {
+
+    if (playerWinCount === computerWinCount) {
+        result = "THE GAME IS A TIE!";
+    }    
+    else if (playerWinCount <= computerWinCount) {
+    result = "COMPUTER WINS THE GAME!"
+    }
+    else if (playerWinCount >= computerWinCount) {
+        result = "PLAYER WINS THE GAME!"
+        }
+
+    return result
+}
+console.log(winner(playerWinCount, computerWinCount))
